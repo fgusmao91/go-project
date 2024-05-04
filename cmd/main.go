7d2a6ff96d8694b62bfa7774dd3609424860ab12
Felipe Gusmão Araújo <felipegusmaoaraujo@MacBook-Pro-de-Felipe.local>
2024-05-04 01:23:05 -0300

    basic web server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Print("Hello Wotld")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "pong")
+	})
+
+	r.Run()
 }
