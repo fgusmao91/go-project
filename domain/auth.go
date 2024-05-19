4d0b522c176026b828731a1926b12fe5816f48da
fgusmao91 <gusmaofelipe91@gmail.com>
2024-05-18 23:46:19 -0300

    adicionada struct de authorizations

--- domain/auth.go
+++ domain/auth.go
@@ -4,3 +4,9 @@ type Credentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+type Authorizations struct {
+	CredentialID int `json:"credential_id"`
+	AppName string `json:"app_name"`
+	AuthType string `json:"auth_type"`
+}
